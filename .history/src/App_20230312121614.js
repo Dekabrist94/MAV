586b5b0a7bec2677553dde6f7e7bf04e57f2c1cb
Alexandru Moloman <dekabriskii94@gmail.com>
2023-03-12 12:23:10 +0100

    <h3> cannot appear at <p> error fixing

---
+++ .history/src/App_20230312121614.js
@@ -0,0 +1,18 @@
+import { Header } from './pages/Header';
+import { PhotoTools } from './pages/PhotoTools';
+import { AboutMe } from './pages/AboutMe';
+import { Portfolio } from './pages/Portfolio';
+import { Contacts } from './pages/Contacts';
+function App() {
+  return (
+    <div className="wrapper">
+      {/* <Header />
+      <AboutMe /> */}
+      <PhotoTools />
+      <Portfolio />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
