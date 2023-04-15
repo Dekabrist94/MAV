5cde2632ea70ecc647a8984af28666bd70e75666
Alexandru Moloman <dekabriskii94@gmail.com>
2023-04-15 02:33:50 +0200

    many things

---
+++ .history/src/App_20230415015059.js
@@ -0,0 +1,18 @@
+import { Header } from './pages/Header/Header';
+import { PhotoTools } from './pages/Cards/PhotoTools';
+import { AboutMe } from './pages/About/AboutMe';
+import { Portfolio } from './pages/MyPorfolio/Portfolio';
+import { Contacts } from './pages/Contacts/Contacts';
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <AboutMe />
+      <PhotoTools />
+      <Portfolio />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
